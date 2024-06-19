a79cf5c01648f9a3b8576443f33afc523603c3c8
smk4411 <saimanojkumar63@gmail.com>
2024-06-19 17:16:16 +0530

    javaworkpus

---
+++ appp.java
@@ -0,0 +1,13 @@
+public class appp {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    
+    
+        String name = "manoj";
+    int age = 29;
+double score= 90;
+System.out.println("Name: " + name);
+System.out.println("age: " + age);
+System.out.println("score: " + score);
+}
+}
